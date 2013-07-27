9ac18cdacce4be8457a713ec2bdaab8185f915d1
hazard_cat <shmirko.konstantin@gmail.com>
2013-07-27 16:53:20 +1100

    New modifications - mie code added

--- ReconstructFDist/MathUtils.f90
+++ ReconstructFDist/MathUtils.f90
@@ -1,4 +1,6 @@
 module MathUtils
+USE mkl95_PRECISION, ONLY: WP => DP
+USE mkl95_LAPACK, ONLY: GELSS
 implicit none
 
 contains
@@ -86,5 +88,53 @@ dr = (r2-r1)/(N-1)
 !Вормируем последовательность в заданных границах
 R = R*dr+r1
 end function linspace_f
+
+
+subroutine tri(Xc, t, B, N)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! создаем 1 треугольную базисную функцию
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+real*8, intent(in)  :: Xc
+integer, intent(in) :: N
+real*8, intent(in)  :: t(N)
+real*8, intent(out) :: B(N)
+integer I
+
+B = max(0.0,1.0-abs(Xc-t))
+
+end subroutine tri
+
+subroutine basisMatrix(B,R,knts,N,M)
+integer, intent(IN) :: N,M
+real*8, intent(IN)  :: R(N), knts(M)
+real*8, intent(OUT) :: B(N,M)
+integer I
+
+! Каждый столбец матрицы - треугольный базисный вектор с центром в узлах knts
+do I=1,M
+    call tri(knts(I), R, B(:,I), N)
+enddo
+
+where (R .LT. knts(1))
+    B(:,1)=0.0
+endwhere
+
+where (R .GT. knts(M))
+    B(:,M)=0.0
+endwhere
+end subroutine basisMatrix
+
+
+subroutine calcCoefs(B,F,C,N,M)
+integer, intent(IN) ::  N,M
+real*8, intent(IN)  ::  B(N,M), F(N,1)
+real*8, intent(OUT) ::  C(M)
+INTEGER RANK, INFO
+REAL*8 S(M), BB(N,M), FF(N,1)
+BB(:,:) = B(:,:)
+FF(:,:) = F(:,:)
+CALL GELSS( BB, FF, RANK, S, 0.00000001_WP, INFO=INFO )
+C = FF(1:M,1)
+end subroutine calcCoefs
 
 endmodule MathUtils
--- ReconstructFDist/ReconstructFDist.f90
+++ ReconstructFDist/ReconstructFDist.f90
@@ -13,13 +13,36 @@
 !****************************************************************************
 
     program ReconstructFDist
-    use MathUtils, ONLY : logspace_s
+    use MathUtils, ONLY : linspace_s, basisMatrix, calcCoefs
+    
+
     implicit none
-    integer, parameter :: N=100
-    real*8 R(N), r1/2.0/, r2/100.0/
+    integer, parameter :: N=100, M=5
+    integer I
+    real*8 R(N), Y(N), r1/1.0/, r2/6.0/, KNTS(M), B(N,M), YY(N,1), C(M)
+
+
+   
+    
+    KNTS = 0
     R=0
-    call logspace_s(r1, r2, R, N-10)
-  
-    print *,R
+    call linspace_s(r1, r2, R, N)
+    call linspace_s(r1, r2-1.0, KNTS,M)
+    call basisMatrix(B, R, KNTS, N,M)
+    
+
+    Y = sin(R*2*3.1415/180.0)
+    do I=1,N
+        write(*,'(7F8.3)') R(I),Y(I),B(I,:)
+    enddo
+
+    
+    YY = RESHAPE(Y, (/N,1/))
+    
+    call calcCoefs(B,YY,C,N,M)
+    
+    do I=1,M
+        print*, knts(I),C(I)
+    enddo
     end program ReconstructFDist
 
